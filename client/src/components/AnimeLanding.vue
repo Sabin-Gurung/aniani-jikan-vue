f518c118a7f638a418bea53205dfe43a181c9f55
Sabin Gurung <Sabin.Gurung@es.ey.com>
2019-10-24 14:50:48 +0200

    grabbind data and conditional rendering done

--- client/src/components/AnimeLanding.vue
+++ client/src/components/AnimeLanding.vue
@@ -1,22 +1,39 @@
 <template>
   <div class="hello">
     <h1>Anime id : {{id}}</h1>
+    <div v-if="animeLoaded">
+      <h1>Anime id : {{anime.title}}</h1>
+    </div>
+    <div v-else>
+      <h1>loading.....</h1>
+    </div>
+
   </div>
 </template>
 
 <script>
+import axios from "axios"
 export default {
   name: 'AnimeLanding',
   props: {
   },
   data(){
     return {
-      id: this.$route.params.id
+      id: this.$route.params.id,
+      animeLoaded : false,
+      anime : null
     }
   },
   methods:{
   },
   mounted(){
+    axios.get(`https://api.jikan.moe/v3/anime/${this.id}`)
+        .then(res => res.data)
+        .then(res => {
+          window.console.log(res);
+          this.anime = res;
+          this.animeLoaded = true;
+        });
   }
 }
 </script>
