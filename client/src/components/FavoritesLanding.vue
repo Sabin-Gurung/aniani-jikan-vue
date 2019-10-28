c8c0177fffa00d44d3563e052207583067151fb6
tejian <sabingurung68@gmail.com>
2019-10-28 21:19:23 +0100

    favorites anime

--- client/src/components/FavoritesLanding.vue
+++ client/src/components/FavoritesLanding.vue
@@ -1,13 +1,37 @@
 <template>
 <div>
-    this is favorites landing page
+    <h3>This are your favorite animes</h3>
+    <div class="row">
+        <anime-mini v-for="anime in favorites" :key="anime.mal_id" :anime="anime"/>
+    </div>
 </div>
   
 </template>
 
 <script>
+import AnimeMini from "./AnimeMini";
+import axios from "axios";
 export default {
-
+    data(){
+        return {
+            favorites : []
+        };
+    },
+    components:{
+        animeMini: AnimeMini
+    },
+    methods : {
+        loadTopAnime(){
+            axios.get("https://api.jikan.moe/v3/top/anime/1")
+            .then(res=> res.data)
+            .then(res=>{
+                this.favorites = res.top;
+            });
+        }
+    },
+    created(){
+        this.loadTopAnime();
+    }
 }
 </script>
 
