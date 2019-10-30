d866f1c845e93745b5c102030928dd27bb4e7ac1
tejian <sabingurung68@gmail.com>
2019-10-30 22:20:26 +0100

    backend setup

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
-const express = require("express")
-const path = require("path")
+const express = require("express");
+const path = require("path");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+
+dotenv.config();
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'client/dist')))
+
+app.use(morgan("short"));
+app.use(express.static(path.join(__dirname, 'client/dist')));
+app.use(express.urlencoded({extended:false}));
+
+app.use("/api", require("./backend/src/handler"));
 
 app.get('*', (req, res)=>{
     res.sendfile(path.join(__dirname, 'client/dist/index.html'));
