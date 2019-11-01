e5d722f22bb69b5b836b3c67e39ea1ed28efe0a6
Sabin Gurung <Sabin.Gurung@es.ey.com>
2019-11-01 17:10:47 +0100

    used necessary middlewares

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const cookie = require("cookie-parser");
+const expSession = require("express-session");
 
 dotenv.config();
 
@@ -10,6 +12,12 @@ const app = express();
 app.use(morgan("short"));
 app.use(express.static(path.join(__dirname, 'client/dist')));
 app.use(express.urlencoded({extended:false}));
+app.use(cookie());
+app.use(expSession({
+    secret : process.env.SESSION_KEY,
+    resave : true,
+    saveUninitialized : true
+}));
 
 app.use("/api", require("./backend/src/handler"));
 
@@ -17,5 +25,5 @@ app.get('*', (req, res)=>{
     res.sendfile(path.join(__dirname, 'client/dist/index.html'));
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3500;
 app.listen(port);
