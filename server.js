65ade13302beb8198874fa629024b8bd33613b64
tejian <sabingurung68@gmail.com>
2019-11-01 22:13:11 +0100

    rename file

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(expSession({
 
 app.use(passport.initialize());
 app.use(passport.session());
-require("./backend/src/passportGoogle").init(passport);
+require("./backend/src/googleAuth").init(passport);
 
 app.use("/api", require("./backend/src/handler"));
 
