47ba09e83ab6b0c67b2db0099ae7ee671021bb3c
tejian <sabingurung68@gmail.com>
2019-11-03 13:26:07 +0000

    added optional in schema

--- backend/src/dao/user_dao.js
+++ backend/src/dao/user_dao.js
@@ -2,11 +2,13 @@
 const mongoose = require("mongoose");
 
 var UserSchema = mongoose.Schema({
-    googleid : {type : Number},
-    username : {type : String}
+    googleid : {type : Number, unique : true},
+    username : {type : String},
+    favorites : {type : [String]}
 });
 
 UserSchema.pre("save", function(done){
+    done();
 });
 
 var UserDao = mongoose.model("Novel", novelSchema);
