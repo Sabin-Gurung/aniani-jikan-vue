03fa0d80bda24e592939b5e00a01e28d087c29de
Sabin Gurung <Sabin.Gurung@es.ey.com>
2019-11-03 01:28:52 +0100

    toggling on favorites

--- client/src/components/FavoritesLanding.vue
+++ client/src/components/FavoritesLanding.vue
@@ -1,6 +1,13 @@
 <template>
 <div>
-    <div class="row">
+    <div v-if="$store.getters.isLoggedOut" class="row m-3">
+        <div class="col-md-3"></div>
+        <div class="col-md-6 alert alert-danger" role="alert">
+            You must be logged in to use the favorites features.
+        </div>
+        <div class="col-md-3"></div>
+    </div>
+    <div v-else class="row">
         <anime-mini v-for="anime in favorites" :key="anime.mal_id" :anime="anime"/>
     </div>
 </div>
