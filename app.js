d95dad3c8c1ad0d00dd5743c96b41694d6974ef9
tejian <sabingurung68@gmail.com>
2019-11-05 22:47:22 +0000

    moved app to separate folder

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+const cookie = require("cookie-parser");
+const expSession = require("express-session");
+const passport = require("passport");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+app.use(morgan("short"));
+app.use(express.urlencoded({extended:false}));
+app.use(cookie());
+app.use(expSession({
+    secret : process.env.SESSION_KEY,
+    resave : true,
+    saveUninitialized : true
+}));
+
+require("./backend/src/mongodb").init();
+
+app.use(passport.initialize());
+app.use(passport.session());
+require("./backend/src/googleAuth")(passport);
+
+require("./backend/src/handler")(app);
+
+if (process.env.ENV == "PROD"){
+    app.use(express.static(path.join(__dirname, 'client/dist')));
+    app.get('*', (req, res)=>{
+        res.sendfile(path.join(__dirname, 'client/dist/index.html'));
+    });
+}
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,40 +1,7 @@
-const express = require("express");
-const path = require("path");
-const dotenv = require("dotenv");
-const morgan = require("morgan");
-const cookie = require("cookie-parser");
-const expSession = require("express-session");
-const passport = require("passport");
 
-dotenv.config();
-
-const app = express();
-
-app.use(morgan("short"));
-app.use(express.urlencoded({extended:false}));
-app.use(cookie());
-app.use(expSession({
-    secret : process.env.SESSION_KEY,
-    resave : true,
-    saveUninitialized : true
-}));
-
-require("./backend/src/mongodb").init();
-
-app.use(passport.initialize());
-app.use(passport.session());
-require("./backend/src/googleAuth")(passport);
-
-require("./backend/src/handler")(app);
-
-if (process.env.ENV == "PROD"){
-    app.use(express.static(path.join(__dirname, 'client/dist')));
-    app.get('*', (req, res)=>{
-        res.sendfile(path.join(__dirname, 'client/dist/index.html'));
-    });
-}
 
 const port = process.env.PORT || 3500;
-app.listen(port, ()=>{
+
+require("./app").listen(port, ()=>{
     console.log(`${process.env.ENV} app started on port http://localhost:${port}`);
 });
