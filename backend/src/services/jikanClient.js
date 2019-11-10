9a2bbf33009bfefe0f8c4073fcd7f15256a88059
tejian <sabingurung68@gmail.com>
2019-11-10 12:01:54 +0000

    removed logging

--- backend/src/services/jikanClient.js
+++ backend/src/services/jikanClient.js
@@ -3,7 +3,6 @@ const API_URL = "https://api.jikan.moe/v3"
 const axios = require("axios").default;
 
 async function getAnime(animeId){
-    console.log("in here");
     try{
         var res = await axios.get(`${API_URL}/anime/${animeId}`)
         return await res.data
