d1bd66aaeb4a5878fcd5bf5650fdb5a53269435f
tejian <sabingurung68@gmail.com>
2019-11-10 10:32:45 +0000

    refactored function

--- backend/src/use_cases/add_favorite_anime.js
+++ backend/src/use_cases/add_favorite_anime.js
@@ -4,21 +4,22 @@ var getUserByGoogleId = require("./add_new_user").getUserByGoogleId;
 const ani_ex = require("../exceptionHandlers");
 const jikanClient = require("../services/jikanClient");
 
-async function execute(userId, animeId){
-    // animeexist
-    console.log("getting anime");
+async function getAnime(animeId){
     var anime = await jikanClient.getAnime(animeId);
-    console.log(anime);
     if (anime == null){
         throw new ani_ex.ResourceNotFoundError(`anime ${animeId} does not exist`);
     }
+    return anime;
+}
 
-    // user exist
+async function execute(userId, animeId){
+    await getAnime(animeId);
+
     var user = await getUserByGoogleId(userId);
     if (user== null){
         throw new ani_ex.ResourceNotFoundError(`user ${userId} does not exist`);
     }
-    // find and save
+
     if (user.favorites.indexOf(animeId) >= 0)
         return user;
     user.favorites.push(animeId);
