b4f306aaedc23d8f1b7ee562d43916aa076b1002
tejian <sabingurung68@gmail.com>
2019-11-10 14:08:40 +0000

    heorku url

--- backend/src/googleAuth.js
+++ backend/src/googleAuth.js
@@ -7,7 +7,7 @@ function init(passport) {
             {
                 clientID: process.env.GOOGLE_CLIENT_ID,
                 clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-                callbackURL: "/api/auth/google/callback"
+                callbackURL: process.env.ENV == "PROD" ? "https://anianijikan-vue.herokuapp.com/api/auth/google/callback" : "/api/auth/google/callback"
             },
             (accessToken, refreshToken, profile, done) => {
                 var userBody = {googleid : profile.id, username : profile.displayName};
